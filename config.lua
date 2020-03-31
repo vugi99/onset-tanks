4d9f04dc3e9a6d1c36df075e86062ea6af6d93ae
vugi99 <voltaism.mail@gmail.com>
2020-03-31 18:26:30 +0200

    v1

---
+++ config.lua
@@ -0,0 +1,56 @@
+reload_time_ms = 3000
+sync_interval_ms = 300
+damage_on_vehicles = 1250
+
+replace_model_start = 43 -- replace_model_start to replace_model_start+3*number_of_tanks will be replaced
+
+tanks = {}
+
+tanks[1] = {
+    pakname = "tank1", -- name of the pak
+    basepath = "tank1base",-- tank base path in the pak
+    cannonpath = "tank1canon",
+    turretpath = "tank1tourelle",
+}
+
+tanks[2] = {
+    pakname = "tank2",
+    basepath = "tank2base",
+    cannonpath = "tank2canon",
+    turretpath = "tank2tourelle",
+}
+
+tanks[3] = {
+    pakname = "tank3",
+    basepath = "tank3base",
+    cannonpath = "tank3canon",
+    turretpath = "tank3tourelle",
+}
+
+tanks[4] = {
+    pakname = "tank4",
+    basepath = "tank4base",
+    cannonpath = "tank4canon",
+    turretpath = "tank4tourelle",
+}
+
+tanks[5] = {
+    pakname = "tank5",
+    basepath = "tank5base",
+    cannonpath = "tank5canon",
+    turretpath = "tank5tourelle",
+}
+
+tanks[6] = {
+    pakname = "tank6",
+    basepath = "tank6base",
+    cannonpath = "tank6canon",
+    turretpath = "tank6tourelle",
+}
+
+tanks[7] = {
+    pakname = "tank7",
+    basepath = "tank7base",
+    cannonpath = "tank7canon",
+    turretpath = "tank7tourelle",
+}
